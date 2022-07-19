ccaaf04ad7f6b518634d09a2270b059ff5c91b6d
Joellensilva <joellen.absilva@gmail.com>
2022-07-19 16:35:27 -0300

    teste de unidade

--- src/data_test.py
+++ src/data_test.py
@@ -4,14 +4,15 @@ import unittest
 
 month = '05'
 year = '2022'
-file_names = [f'/membros-ativos-contracheque-{month}-{year}.xls', f'/membros-ativos-indenizatorias-{month}-{year}.xls']
+file_names = [f'src/output_test/sheets/membros-ativos-contracheque-{month}-{year}.xls',
+              f'src/output_test/sheets/membros-ativos-indenizatorias-{month}-{year}.xls']
 
 
 class TestData(unittest.TestCase):
     def test_validate_existence(self):
         STATUS_DATA_UNAVAILABLE = 4
         with self.assertRaises(SystemExit) as cm:
-            dados = load(file_names, year, month, "/tmp/coletormpap/output")
+            dados = load(file_names, year, month, "")
             dados.validate("")
         self.assertEqual(cm.exception.code, STATUS_DATA_UNAVAILABLE)
 
